59ce607e4d73969c5151f08cd323a26da1d86d28
Anderson Dias <andersondiasmd25@gmail.com>
2024-03-23 14:43:48 -0300

    configurando a conexão do front end com back e mudando as portas 4001

---
+++ src/pages/CadastroMedico/index.tsx
@@ -0,0 +1,13 @@
+// src/pages/CadastroMedico/index.tsx
+import FormularioMedico from '../../components/FormularioMedico';
+
+function CadastroMedico() {
+  return (
+    <div>
+      <h1>Cadastro de Médico</h1>
+      <FormularioMedico />
+    </div>
+  );
+}
+
+export default CadastroMedico;
